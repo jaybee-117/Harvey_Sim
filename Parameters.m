6d6a11c89978bc311389597ea013b1160e594905
jaybee-117 <unofficialme117@gmail.com>
2022-11-22 06:20:34 +0530

    Fixed supply model and added delay attack

--- Parameters.m
+++ Parameters.m
@@ -5,7 +5,7 @@ consumers = 1405;
 consumer_no = 45;
 lambda_0 = 21;
 p = 0.04363;
-n = 0.4;
+n = 0.9;
 q = 1.287;
 mean_d = 7;
 var_d = 3.5*3.5;
@@ -19,17 +19,19 @@ W = 0;
 for i = 1:consumers
     W = W + d(i)*lambda_0^e(i);
 end
-E = e(consumer_no);
+E = mean_e;
 D = W/(lambda_0^E);
 
 G_w = D*E*lambda_0^(E-1);
-G_s = p;
+G_s = p*consumers;
 a = 2*n/(G_s - G_w);
 
-s_0 = q;
+s_0 = q*consumers;
 w_0 = D*(1-E)*lambda_0^E;
-b_k = 0.289*1405;
+b_k = 0.289*consumers;
 B = s_0 - w_0 - b_k;
+h = abs(G_w/G_s);
+y = ((1-n+h)/(h*n))^(-E);
 %%--------------------------------Plots----------------------------------%%
 figure(1)
 plot(d);
